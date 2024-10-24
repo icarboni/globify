240b9b2db30083c3457bce60d0c25dc768828973
Ilenia Carboni <icarboni@c1r10s1.42malaga.com>
2024-10-24 15:51:03 +0200

    remove outline from search bar

--- app/components/Navbar/index.tsx
+++ app/components/Navbar/index.tsx
@@ -51,7 +51,7 @@ const Navbar = () => {
               <FaSearch className="ms-2" />
               <input
                 id="search-form"
-                className="me-2 bg-gray-700 text-white rounded-full px-4 py-2"
+                className="me-2 bg-gray-700 text-white rounded-full px-4 py-2 focus: outline-none"
                 type="text"
                 placeholder="Search"
                 aria-label="Search"
