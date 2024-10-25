04686cc4bb46131bab1142d0077adba43ee08416
Ilenia Carboni <icarboni@MBP-CC9KCQN6P4.local>
2024-10-25 14:02:13 +0200

    fix build error

--- app/callback/page.ts
+++ app/callback/page.ts
@@ -1,16 +1,27 @@
 "use client";
 
-export default function Callback() {
-  const url = new URL(window.location.href);
+import { useEffect } from "react";
 
-  const hash = url.hash;
-  if (hash) {
-    const params = new URLSearchParams(hash.substring(1));
-    const accessToken = params.get("access_token");
-    if (accessToken) {
-      localStorage.setItem("accessToken", accessToken);
+const Callback = () => {
+
+
+  useEffect(() => {
+    const getAccessToken = () => {
+      const url = new URL(window.location.href);
+    
+      const hash = url.hash;
+      if (hash) {
+        const params = new URLSearchParams(hash.substring(1));
+        const accessToken = params.get("access_token");
+        if (accessToken) {
+          localStorage.setItem("accessToken", accessToken);
+        }
+    
+        window.location.href = "http://localhost:3000";
+      }
     }
-
-    window.location.href = "http://localhost:3000";
-  }
+    getAccessToken();
+  })
 }
+
+export default Callback;
